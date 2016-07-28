ec212895658f36a7b5477719440f2cfb09898377
zburgermeiszter <zoltan@burgermeiszter.com>
2016-07-28 20:10:18 +0100

    Syncthing inotify with docker compose

---
+++ syncthing/syncthing-inotify/amd64/start.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+CONFIG_FILE=${CONFIG_FILE:-/syncthing/.config/syncthing/config.xml}
+APIKEY=${APIKEY:-$(cat $CONFIG_FILE | awk -F "[><]" '/apikey/{print $3}')}
+PROTOCOL=$(grep -q "tls=\"true\"" $CONFIG_FILE && echo "https" || echo "http")
+ST_HOST=${ST_HOST:-syncthing}
+ST_PORT=${ST_PORT:-8384}
+
+while true
+do
+  syncthing-inotify -target="$PROTOCOL://$ST_HOST:$ST_PORT" -api=$APIKEY
+done;
---
+++ syncthing/syncthing/amd64/start.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+CONFIG_PATH=$HOME/.config/syncthing
+CONFIG_FILE=$CONFIG_PATH/config.xml
+
+setup() {
+  syncthing -generate=$CONFIG_PATH
+  sed -i 's/127.0.0.1/0.0.0.0/g' $CONFIG_FILE
+}
+
+if [ ! -f $CONFIG_FILE ]; then
+    echo "Config not found! Generating..."
+    setup
+fi
+
+syncthing -no-browser
